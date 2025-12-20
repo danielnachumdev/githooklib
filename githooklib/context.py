4dc57626c9da9cc58fdaae8c0e3f908225e6bae2
danielnachumdev <danielnachumdev@gmail.com>
2025-12-20 11:24:42 +0200

    re-added project root into the context object
    Took 3 minutes

--- githooklib/context.py
+++ githooklib/context.py
@@ -1,7 +1,9 @@
 import sys
 from dataclasses import dataclass
+from pathlib import Path
 from typing import List
 
+from .gateways import ProjectRootGateway
 from .logger import get_logger
 
 logger = get_logger()
@@ -11,6 +13,7 @@ logger = get_logger()
 class GitHookContext:
     hook_name: str
     argv: List[str]
+    project_root: Path = ProjectRootGateway.find_project_root()
 
     @classmethod
     def from_argv(cls, hook_name: str) -> "GitHookContext":
